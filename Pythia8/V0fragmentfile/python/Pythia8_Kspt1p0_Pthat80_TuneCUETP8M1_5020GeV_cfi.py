37a234b57410f59f07c608432300353174e46bbb
Lingshan <xulingshan@gmail.com>
2015-11-04 09:56:50 +0100

    add ks fragment

---
+++ Pythia8/V0fragmentfile/python/Pythia8_Kspt1p0_Pthat80_TuneCUETP8M1_5020GeV_cfi.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(5020.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(     
+            'HardQCD:all = on',
+            'PhaseSpace:pTHatMin = 80.', #min pthat
+        ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+        )
+    )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+
+#
+
+V0filter = cms.EDFilter("MCSingleParticleFilter",
+    MaxEta = cms.untracked.vdouble(2.4, 2.4),
+    MinEta = cms.untracked.vdouble(-2.4, -2.4),
+    MinPt = cms.untracked.vdouble(1.0, 1.0), #min pt
+    ParticleID = cms.untracked.vint32(310, -310)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*V0filter)
